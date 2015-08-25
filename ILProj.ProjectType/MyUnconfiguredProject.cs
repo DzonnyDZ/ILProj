3554944b9c8dd80ab93f9f247f0e1f088c9bfa0a
Jan Záruba <jan.zaruba7393@embedit.cz>
2015-08-25 10:33:12 +0200

    fixes

--- ILProj.ProjectType/MyUnconfiguredProject.cs
+++ ILProj.ProjectType/MyUnconfiguredProject.cs
@@ -35,7 +35,7 @@ namespace Dzonny.ILProj
         /// The file extension used by your project type.
         /// This does not include the leading period.
         /// </summary>
-        internal const string ProjectExtension = "jsnproj";
+        internal const string ProjectExtension = "ilproj";
 
         /// <summary>
         /// A project capability that is present in your project type and none others.
